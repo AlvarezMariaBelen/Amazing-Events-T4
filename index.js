19ae18dc5f203d35d61052c80e846a2a4416d63b
AlvarezMariaBelen <beluchis1991@gmail.com>
2022-10-23 17:09:08 -0300

    index.js

--- index.js
+++ index.js
@@ -1,38 +1,23 @@
-fetch("https://mind-hub.up.railway.app/amazing");
+let arrayFromAPI;
+let arrayEvents = [];
+async function getDataFromApi() {
+  await fetch("https://mind-hub.up.railway.app/amazing")
+    .then((resp) => resp.json())
+    .then((json) => (arrayFromAPI = json));
+  arrayEvents = arrayFromAPI.events;
 
-async function fetchApi() {
-  //funcion asincrona
-  try {
-    //try hace la petición
-    let response = await fetch("https://mind-hub.up.railway.app/amazing"); //fetch sirve para consultar los datos desde Api y await espera la respuesta de la promesa
-    //response es la respuesta de la peticion fetch
-    let data = await response.json();
-    //data es la transformacion de la peticion
-    let events = data.events;
-    printCard(events);
-    printChecks();
-    searchbar();
-    checkb();
-  } catch (error) {
-    //catch cachea el error si lo hay
-    console.log("No se puede mostrar lo solicitado");
-  }
-}
-fetchApi();
+  let cardStorer = document.getElementById("container");
+  let checkStorer = document.getElementById("check");
+  let checks = arrayEvents.map((event) => event.category);
+  let filterrepeat = new Set(checks);
+  let categorysCheck = [...filterrepeat];
 
-let dataEvents = data.events;
-let cardStorer = document.getElementById("container");
-let checkStorer = document.getElementById("check");
-let checks = dataEvents.map((event) => event.category);
-let filterrepeat = new Set(checks);
-let categorysCheck = [...filterrepeat];
-
-//creo las cards dinamicas
-function printCard(array) {
-  let locationCards = " ";
-  if (array.length !== 0) {
-    array.forEach((data) => {
-      locationCards += ` 
+  //creo las cards dinamicas
+  function printCard(array) {
+    let locationCards = " ";
+    if (array.length !== 0) {
+      array.forEach((data) => {
+        locationCards += ` 
        <div class="cards">
             <img src="${data.image}" />
          <div class="upcard">  
@@ -45,89 +30,93 @@ function printCard(array) {
          </div>   
         </div>
         `;
-      cardStorer.innerHTML = locationCards;
-    });
-  } else {
-    cardStorer.innerHTML = `
+        cardStorer.innerHTML = locationCards;
+      });
+    } else {
+      cardStorer.innerHTML = `
     <div>
       <p class="card-text">No results found!</p>
     </div>
   `;
+    }
   }
-}
-//printCard(dataEvents);
+  printCard(arrayEvents);
 
-//checks dinamicos
-function printChecks() {
-  let cheksLocation = "";
-  categorysCheck.forEach((category) => {
-    cheksLocation += `<label><input type="checkbox" value="${category}">${category}</label>`;
-  });
-  checkStorer.innerHTML = cheksLocation;
-}
-//printChecks();
+  //checks dinamicos
+  function printChecks() {
+    let cheksLocation = "";
+    categorysCheck.forEach((category) => {
+      cheksLocation += `<label><input type="checkbox" value="${category}">${category}</label>`;
+    });
+    checkStorer.innerHTML = cheksLocation;
+  }
+  printChecks();
 
-let checkboxBranded = [];
-let textSearch = "";
-let checkbox = document.querySelectorAll("input[type=checkbox]");
-function checkb() {
-  checkbox.forEach((check) =>
-    check.addEventListener("click", (event) => {
-      let checked = event.target.checked;
-      if (checked) {
-        checkboxBranded.push(event.target.value);
-        crossfilter();
-      } else {
-        checkboxBranded = checkboxBranded.filter(
-          (uncheck) => uncheck !== event.target.value
+  let checkboxBranded = [];
+  let textSearch = "";
+  let checkbox = document.querySelectorAll("input[type=checkbox]");
+  function checkb() {
+    checkbox.forEach((check) =>
+      check.addEventListener("click", (event) => {
+        let checked = event.target.checked;
+        if (checked) {
+          checkboxBranded.push(event.target.value);
+          crossfilter();
+        } else {
+          checkboxBranded = checkboxBranded.filter(
+            (uncheck) => uncheck !== event.target.value
+          );
+          crossfilter();
+        }
+      })
+    );
+  }
+  checkb();
+
+  //defino la barra de busqueda
+  let search = document.getElementById("searchInput");
+  function searchbar() {
+    search.addEventListener("input", (events) => {
+      textSearch = events.target.value;
+      crossfilter();
+    });
+  }
+  searchbar();
+
+  //filtro cruzado
+  function crossfilter() {
+    let info = [];
+    if (checkboxBranded.length > 0 && textSearch !== "") {
+      checkboxBranded.map((selected) => {
+        info.push(
+          ...arrayEvents.filter(
+            (events) =>
+              events.name
+                .toLocaleLowerCase()
+                .includes(textSearch.trim().toLocaleLowerCase()) &&
+              events.category.includes(selected)
+          )
         );
-        crossfilter();
-      }
-    })
-  );
-}
-
-//defino la barra de busqueda
-let search = document.getElementById("searchInput");
-function searchbar() {
-  search.addEventListener("input", (events) => {
-    textSearch = events.target.value;
-    crossfilter();
-  });
-}
-
-//filtro cruzado
-function crossfilter() {
-  let info = [];
-  if (checkboxBranded.length > 0 && textSearch !== "") {
-    checkboxBranded.map((selected) => {
+      });
+    } else if (checkboxBranded.length > 0 && textSearch === "") {
+      checkboxBranded.map((selected) => {
+        info.push(
+          ...arrayEvents.filter((events) => events.category.includes(selected))
+        );
+      });
+    } else if (checkboxBranded.length == 0 && textSearch !== "") {
       info.push(
-        ...dataEvents.filter(
-          (events) =>
-            events.name
-              .toLocaleLowerCase()
-              .includes(textSearch.trim().toLocaleLowerCase()) &&
-            events.category.includes(selected)
+        ...arrayEvents.filter((events) =>
+          events.name
+            .toLocaleLowerCase()
+            .includes(textSearch.trim().toLocaleLowerCase())
         )
       );
-    });
-  } else if (checkboxBranded.length > 0 && textSearch === "") {
-    checkboxBranded.map((selected) => {
-      info.push(
-        ...dataEvents.filter((events) => events.category.includes(selected))
-      );
-    });
-  } else if (checkboxBranded.length == 0 && textSearch !== "") {
-    info.push(
-      ...dataEvents.filter((events) =>
-        events.name
-          .toLocaleLowerCase()
-          .includes(textSearch.trim().toLocaleLowerCase())
-      )
-    );
-  } else {
-    info.push(...dataEvents);
+    } else {
+      info.push(...arrayEvents);
+    }
+    printCard(info);
   }
-  printCard(info);
+  crossfilter();
 }
-//crossfilter();
+getDataFromApi();
